1e0dd13ccda9e45f4ac33debf8c2a95e5d38b805
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2021-10-20 08:11:48 +0545

    lambda fn config added for sending request towards deep;

--- modules/sqs_lambda/main.tf
+++ modules/sqs_lambda/main.tf
@@ -60,7 +60,7 @@ module "input_request_fn" {
     }
 }
 
-resource "aws_lambda_event_source_mapping" "sqs_to_lambda_trigger" {
+resource "aws_lambda_event_source_mapping" "sqs_to_extract_lambda_trigger" {
   event_source_arn = aws_sqs_queue.input_queue.arn
   function_name    = module.extract_docs_fn.lambda_function_arn
 }
@@ -146,4 +146,42 @@ module "extract_docs_fn" {
         DEST_S3_BUCKET = "${var.processed_docs_bucket}"
         PROCESSED_QUEUE = aws_sqs_queue.processed_queue.id
     }
+}
+
+module "output_request_fn" {
+    source = "terraform-aws-modules/lambda/aws"
+
+    function_name = "te-output-request-func-${var.environment}"
+    handler = "app.output_request"
+    runtime = "python3.8"
+    timeout = 30
+
+    source_path = [
+    {
+        path = "${path.module}/../../code/output_request"
+    }
+    ]
+
+    attach_policy_json    = true
+
+    policy_json = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+    {
+        "Effect": "Allow",
+        "Action": [
+            "sqs:ReceiveMessage",
+            "sqs:DeleteMessage",
+            "sqs:GetQueueAttributes",
+            "s3:GetObject"
+        ],
+        "Resource": ["*"]
+    }
+    ]
+    })
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_to_output_lambda_trigger" {
+  event_source_arn = aws_sqs_queue.processed_queue.arn
+  function_name    = module.output_request_fn.lambda_function_arn
 }
