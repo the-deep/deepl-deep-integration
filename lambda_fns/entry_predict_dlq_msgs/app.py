5091ac7b6470446bb8227654ec2b30d73c9b0be3
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2021-12-27 13:54:11 +0545

    lambda fn to put back to queue when message are not successfully delivered on callback fn;

---
+++ lambda_fns/entry_predict_dlq_msgs/app.py
@@ -0,0 +1,68 @@
+import os
+import boto3
+
+SQS_MSG_DELAY_SECS = 600
+
+DEFAULT_AWS_REGION = "us-east-1"
+
+AWS_REGION = os.environ.get("AWS_REGION", DEFAULT_AWS_REGION)
+PREDICTION_QUEUE_NAME = os.environ.get("PREDICTION_QUEUE")
+
+sqs_client = boto3.client('sqs', region_name=AWS_REGION)
+
+
+def send_message2sqs(
+    entry_id,
+    entry,
+    predictions,
+    callback_url
+):
+    message_attributes = {}
+    message_attributes['entry'] = {
+        'DataType': 'String',
+        'StringValue': entry
+    }
+    if predictions:
+        message_attributes['predictions'] = {
+            'DataType': 'String',
+            'StringValue': predictions  # already serialized.
+        }
+    if callback_url:
+        message_attributes['callback_url'] = {
+            'DataType': 'String',
+            'StringValue': callback_url
+        }
+
+    if PREDICTION_QUEUE_NAME:
+        sqs_client.send_message(
+            QueueUrl=PREDICTION_QUEUE_NAME,
+            MessageBody=entry_id,
+            DelaySeconds=SQS_MSG_DELAY_SECS,
+            MessageAttributes=message_attributes
+        )
+    else:
+        print("Message not sent to the processed SQS.")
+
+
+def entry_predict_dlq_msgs_handler(event, context):
+    records = event['Records']
+
+    for record in records:
+        entry_id = record['body']
+        entry = record['messageAttributes']['entry']['stringValue']
+        predictions = record['messageAttributes']['predictions']['stringValue']
+        callback_url = record['messageAttributes']['callback_url']['stringValue']
+
+        print(f"Sending the entry id {entry_id} message to Processing Queue")
+
+        sqs_message = {
+            'entry_id': entry_id,
+            'entry': entry,
+            'predictions': predictions,
+            'callback_url': callback_url
+        }
+        send_message2sqs(**sqs_message)
+
+    return {
+        'statusCode': 200
+    }
