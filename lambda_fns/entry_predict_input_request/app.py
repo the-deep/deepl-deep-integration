2fa899658fa949465d5e5eaee92f917d555cc2e9
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2022-02-08 16:17:52 +0545

    try block improvement + code cleanup;

--- lambda_fns/entry_predict_input_request/app.py
+++ lambda_fns/entry_predict_input_request/app.py
@@ -15,42 +15,37 @@ sqs_client = boto3.client("sqs", region_name=aws_region)
 
 
 def entry_msg_sqs_handler(event, context):
-    errormsg = None
+    request_body = json.loads(event['body'])
+    entries_lst = request_body['entries']
+    callback_url = request_body['callback_url']
 
-    if queue_url:
-        request_body = json.loads(event['body'])
-        entries_lst = request_body['entries']
-        callback_url = request_body['callback_url']
-
-        try:
-            entries = [{
-                'Id': str(uuid.uuid4()),
-                'MessageBody': item['entry_id'],
-                'DelaySeconds': 0,
-                'MessageAttributes': {
-                    'entry': {
-                        'DataType': 'String',
-                        'StringValue': item['entry']
-                    },
-                    'callback_url': {
-                        'DataType': 'String',
-                        'StringValue': callback_url
-                    }
-                }
-            } for item in entries_lst]
-            sqs_client.send_message_batch(
-                QueueUrl=queue_url,
-                Entries=entries
-            )
-            return {
-                'statusCode': 200,
-                'body': 'Entries Enqueued Successfully'
+    entries = [{
+        'Id': str(uuid.uuid4()),
+        'MessageBody': item['entry_id'],
+        'DelaySeconds': 0,
+        'MessageAttributes': {
+            'entry': {
+                'DataType': 'String',
+                'StringValue': item['entry']
+            },
+            'callback_url': {
+                'DataType': 'String',
+                'StringValue': callback_url
             }
-        except Exception as err:
-            logging.error(err)
-            errormsg = err
-
-    return {
-        'statusCode': 500,
-        'body': errormsg
-    }
+        }
+    } for item in entries_lst]
+    try:
+        sqs_client.send_message_batch(
+            QueueUrl=queue_url,
+            Entries=entries
+        )
+        return {
+            'statusCode': 200,
+            'body': 'Entries Enqueued Successfully'
+        }
+    except Exception as err:
+        logging.error(err)
+        return {
+            'statusCode': 500,
+            'body': err
+        }
